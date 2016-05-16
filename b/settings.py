bdd2d3c6fe960703430b26c20d54ab2f9fb25641
carzooma <carzooma@gmail.com>
2016-05-16 13:44:52 -0400

    Set up polls
    Beginning part 3 now

--- b/settings.py
+++ b/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
